05fca62c30969230e73bc34bd482b6bc3e0a92ac
boquer <dani_981103@hotmail.com>
2020-03-13 14:21:07 -0600

    archivos arriba

---
+++ js/inicio_sesion.js
@@ -0,0 +1,13 @@
+//tomar el id "login" y cuando se haga "click" se llamará la función "ingresar"
+document.querySelector('#login').addEventListener('click',ingresar)
+
+
+function ingresar(){
+  //Se crean las variables donde quedará el usuario y contraseña que ingrese la persona.
+  var usuario = document.getElementById('usuario').value; //.value es para tomar el valor que se ingrese, trabajar con el.
+  var pasword = document.getElementById('contraseña').value;
+
+  if(usuario=='boquer' && pasword=='12345'){
+    window.location.href = 'inicio.html';
+  }
+}
